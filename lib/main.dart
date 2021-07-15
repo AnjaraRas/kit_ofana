0bd134e0d48afa1bfe3e9881f4b4ae0e63b92d68
Anjara <anjara.itendro@gmail.com>
2021-07-15 12:57:31 +0200

    change white to green

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primaryColor: Colors.white,
+        primaryColor: Colors.green,
       ),
       home: HomePage(),
     );
